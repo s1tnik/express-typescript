148299ef598530d0db3dd2adc50987cea62dc5c8
Viacheslav Sitnik <slava2261812@gmail.com>
2023-03-13 16:56:07 +0300

    add error handling for analyze controller

--- src/controllers/analyzeController.ts
+++ src/controllers/analyzeController.ts
@@ -4,7 +4,11 @@ import { analyzeRepository } from "../utils/analyzeRepository";
 export const analyzeController = express.Router();
 
 analyzeController.post("/framework", async (req, res) => {
-    const { SourceType, Link } = req.body;
+  const { SourceType, Link } = req.body;
+  try {
     const result = await analyzeRepository(SourceType, Link);
     res.status(200).json(result);
+  } catch ({ message }) {
+    res.status(400).json({ message: message ?? "error" });
+  }
 });
